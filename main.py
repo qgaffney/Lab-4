ca4a3f6ecf2f30a7910b3d913c473bc2252e78d1
qgaffney <90237467+qgaffney@users.noreply.github.com>
2024-11-10 16:12:13 -0500

    lab-4

--- main.py
+++ main.py
@@ -1,46 +1,47 @@
 import csv
+from datetime import datetime
 
-def selection_sort(arr):
-    for i in range(len(arr)):
-        min_idx = i
-        for j in range(i + 1, len(arr)):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-    return arr
+def selection_sort(data, key):
+    for i in range(len(data)):
+        min_index = i
+        for j in range(i + 1, len(data)):
+            if data[j][key] < data[min_index][key]:
+                min_index = j
+        data[i], data[min_index] = data[min_index], data[i]
 
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
+def insertion_sort(data, key):
+    for i in range(1, len(data)):
+        key_item = data[i][key]
         j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
+        while j >= 0 and data[j][key] > key_item:
+            data[j + 1] = data[j]
             j -= 1
-        arr[j + 1] = key
-    return arr
+        data[j + 1] = data[i]
 
-def read_csv(filename):
-    with open(filename, 'r') as file:
-        reader = csv.reader(file)
+def sort_csv_date(csv_file, date_field, sort_method = 'selection'):
+    with open(csv_file, 'r') as file:
+        reader = csv.DictReader(file)
         data = list(reader)
-    return data
 
-def sort_csv(filename, sort_function, column_index):
-    data = read_csv(filename)
-    header = data[0]
-    data = data[1:]
+        for row in data:
+            row[date_field] = datetime.strptime(row[date_field], '%b-%d')
 
-    data.sort(key=lambda row: row[column_index])
+        if sort_method == 'selection':
+            selection_sort(data, date_field)
+        elif sort_method == 'insertion':
+            insertion_sort(data, date_field)
+        else:
+            raise ValueError("Invalid. Select 'selection' or 'insertion'.")
+        
+        for row in data:
+            row[date_field] = row[date_field].strftime('%b-%d')
 
-    with open(filename, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(header)
+    with open('sorted_' + csv_file, 'w', newline = '') as file:
+        writer = csv.DictWriter(file, fieldnames=reader.fieldnames)
+        writer.writeheader()
         writer.writerows(data)
 
-if __name__ == "__main__":
-    filename = "/Users/q.gaffney/Lab-4/Lab 4/Resources/budget_data.csv"
-    column_index = 1
-
-    sort_csv(filename, selection_sort, column_index)
-
-    sort_csv(filename, insertion_sort, column_index)
+if __name__ == '__main__':
+    csv_file = '/Users/q.gaffney/Lab-4/Resources/budget_data.csv'
+    date_field = 'Date'
+    sort_csv_date(csv_file, date_field, 'insertion')
